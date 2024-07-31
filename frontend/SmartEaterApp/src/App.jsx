786defcb931e45749695c4755ba7c3d1c7df78ab
mt-evan <10evantardiff@gmail.com>
2024-07-30 20:09:43 -0700

    commit
    trying to get the path/url to /smart-eater once signed in

--- frontend/SmartEaterApp/src/App.jsx
+++ frontend/SmartEaterApp/src/App.jsx
@@ -1,48 +1,40 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import './App.css'
-
-import IntroPage from './components/IntroPage/IntroPage'
-import SmartEaterPage from './components/SmartEaterPage/SmartEaterPage';
-
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<IntroPage />} />
-        <Route path="/smart-eater" element={<SmartEaterPage />} />
-      </Routes>
-    </Router>
-  )
-}
-
-export default App
-
-/*
-
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-import './App.css'
+import './App.css';
 
-import IntroPage from './components/IntroPage/IntroPage'
+import IntroPage from './components/IntroPage/IntroPage';
 import SmartEaterPage from './components/SmartEaterPage/SmartEaterPage';
-
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<SignedOut><IntroPage /></SignedOut>} />
-        <Route path="/smart-eater" element={<SignedIn><SmartEaterPage /></SignedIn>} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <div>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <SignedOut>
+                <IntroPage />
+              </SignedOut>
+            }
+          />
+          <Route
+            path="/smart-eater"
+            element={
+              <SignedIn>
+                <SmartEaterPage />
+              </SignedIn>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
     </Router>
-  )
+  );
 }
 
 export default App;
-
-*/
--- frontend/SmartEaterApp/src/components/IntroPage/mini/Button/Button.jsx
+++ frontend/SmartEaterApp/src/components/IntroPage/mini/Button/Button.jsx
@@ -1,34 +1,8 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable react/no-unescaped-entities */
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Button.module.css';
-
-function Button() {
-    return (
-        <section className={styles.container} id="smarteater">
-            <div className={styles.info}>
-                <p className={styles.description}>
-                    Explore our SmartEater website! The button below will take you to our WebApp, where you can enter your meals, track your diet, and receive personalized meal recommendations to help you maintain a balanced and healthy lifestyle.
-                </p>
-                <Link to="/smart-eater">
-                    <button type="button" className={styles.button}>
-                        Go to SmartEater WebApp
-                    </button>
-                </Link>
-            </div>
-        </section>
-    );
-}
-
-export default Button;
-
-/*
-
 import React from 'react';
 import { SignInButton } from '@clerk/clerk-react';
 import styles from './Button.module.css';
+
 
 function Button() {
     return (
@@ -39,7 +13,7 @@ function Button() {
                 </p>
                 <SignInButton mode="modal">
                     <button type="button" className={styles.button}>
-                        Go to SmartEater WebApp
+                        Sign up / Log in
                     </button>
                 </SignInButton>
             </div>
@@ -48,5 +22,3 @@ function Button() {
 }
 
 export default Button;
-
-*/
