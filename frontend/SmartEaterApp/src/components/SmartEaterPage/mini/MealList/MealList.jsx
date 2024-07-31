a1b52bd3630e48d4a1d14f016173d1d74eda2c17
mt-evan <10evantardiff@gmail.com>
2024-07-31 01:57:25 -0700

    dummy code for mealList

--- frontend/SmartEaterApp/src/components/SmartEaterPage/mini/MealList/MealList.jsx
+++ frontend/SmartEaterApp/src/components/SmartEaterPage/mini/MealList/MealList.jsx
@@ -1,14 +1,40 @@
 /* eslint-disable no-unused-vars */
-import { Link } from 'react-router-dom';
+import styles from './MealList.module.css';
 
 function MealList() {
-
     return (
-        <div>
-            <h3>Helloooooo</h3>
+        <div className={styles.container}>
+            <button className={`btn btn-primary ${styles.button}`}>Generate Next Meal Options</button>
+            <div className={styles.mealListContainer}>
+                <ul className={styles.mealList}>
+                    <li className={styles.mealListItem}>
+                        <p>Meal 1</p>
+                        <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </li>
+                    <li className={styles.mealListItem}>
+                        <p>Meal 2</p>
+                        <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </li>
+                    <li className={styles.mealListItem}>
+                        <p>Meal 3</p>
+                        <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </li>
+                    <li className={styles.mealListItem}>
+                        <p>Meal 4</p>
+                        <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </li>
+                    <li className={styles.mealListItem}>
+                        <p>Meal 5</p>
+                        <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </li>
+                    <li className={styles.mealListItem}>
+                        <p>Meal 6</p>
+                        <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </li>
+                </ul>
+            </div>
         </div>
-
-    )
+    );
 }
 
 export default MealList;
