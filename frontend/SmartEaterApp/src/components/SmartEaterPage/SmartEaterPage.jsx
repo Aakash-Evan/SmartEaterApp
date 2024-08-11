fc7fd3703b172077291d14ec3883cf86eeff3ddc
mt-evan <10evantardiff@gmail.com>
2024-08-11 02:06:25 -0700

    made a home page for SmartEaterApp
    Click logo in header to go back to Home

--- frontend/SmartEaterApp/src/components/SmartEaterPage/SmartEaterPage.jsx
+++ frontend/SmartEaterApp/src/components/SmartEaterPage/SmartEaterPage.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable no-unused-vars */
 import { Route, Routes, Navigate } from 'react-router-dom';
-
+import styles from './SmartEaterPage.module.css';
 import Header from './mini/Header/Header';
+import Home from './mini/Home/Home';
 import MealList from './mini/MealList/MealList';
 import MealLogger from './mini/MealLogger/MealLogger';
 import MealRecords from './mini/MealRecords/MealRecords';
 
 function SmartEaterPage() {
     return (
-        <div>
+        <div className={styles.container}>
             <Header />
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/meal-logger" element={<MealLogger />} />
                 <Route path="/meal-list" element={<MealList />} />
                 <Route path="/meal-records" element={<MealRecords />} />
--- frontend/SmartEaterApp/src/components/SmartEaterPage/mini/Header/Header.jsx
+++ frontend/SmartEaterApp/src/components/SmartEaterPage/mini/Header/Header.jsx
@@ -3,13 +3,19 @@
 import styles from './Header.module.css';
 import { getImageUrl } from '../../utils';
 import { UserButton } from '@clerk/clerk-react';
+import { useNavigate } from 'react-router-dom';
 
 
 function Header() {
+    const navigate = useNavigate();
+
+    const handleClick = (path) => {
+        navigate(path);
+    };
     return (
         <nav className={styles.header}>
             <div className={styles.logo}>
-                <img src={getImageUrl('logo.png')} alt="Logo" />
+                <img src={getImageUrl('logo.png')} alt="Logo" onClick={() => handleClick('/smart-eater')} />
             </div>
             <div className={styles.nav}>
                 <UserButton className={styles.userButton}/>
